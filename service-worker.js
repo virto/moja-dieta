ca49555b4076e566a0b26a4a81234b7aafca6c5f
Arek <virto@o2.pl>
2026-06-19 19:40:59 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'moja-dieta-v4';
+const CACHE = 'moja-dieta-v5';
 const APP_SHELL = ['/', '/index.html', '/styles.css', '/app.js', '/manifest.webmanifest', '/icons/icon-192.png', '/icons/icon-512.png'];
 
 self.addEventListener('install', (event) => {
